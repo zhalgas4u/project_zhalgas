c2d1c2d91c8fe7c3f65cfa932fe1312fa4a7dde4
zhalgas4u <zhalgas4u@mail.ru>
2024-11-26 17:01:19 +0600

    myfirstproject

---
+++ src/main/java/com/example/MyFirstProjectWithMentor/MyFirstProjectWithMentorApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstProjectWithMentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//fffffffffffffffffffff
+@SpringBootApplication
+public class MyFirstProjectWithMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectWithMentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyFirstProjectWithMentor/MyFirstProjectWithMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstProjectWithMentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectWithMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
